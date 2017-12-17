365b633b14d28e6e855314f686c31d413e637972
“B3zcz31nosc <tomczyk.a@outlook.com>
2017-12-17 21:06:25 +0100

    rep

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ForumUser {
+    private final String forumUser;
+
     public ForumUser() {
+        this.forumUser = "John Smith";
     }
 
     public static String getName() {
+
         return "John Smith";
     }
 }
